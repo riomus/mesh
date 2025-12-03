4ebfeb2e9cbdea69312348bbd4fdb1b25655b1e7
Roman Bartusiak <riomus@gmail.com>
2025-12-03 12:57:23 +0100

    fix: gradle syntax fix for desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        coreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
